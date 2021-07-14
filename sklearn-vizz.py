640bae49d92cdc41740dc4d47943aba4ee498bdd
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-14 10:44:26 +0800

    Update sklearn-vizz.py

--- sklearn-vizz.py
+++ sklearn-vizz.py
@@ -21,6 +21,7 @@ from sklearn.metrics import classification_report
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            #footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
